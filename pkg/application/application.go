4b7d212d691ee7bf1bba46e6802374237d6d3bdf
Clint Berry <clint@getweave.com>
2017-07-31 23:23:52 -0600

    very early objects

---
+++ pkg/application/application.go
@@ -0,0 +1,11 @@
+package application
+
+// Application is the struct for the base application in ploio
+type Application struct {
+	Name      string
+	Owner     string
+	Namespace string
+}
+
+type Service struct {
+}
